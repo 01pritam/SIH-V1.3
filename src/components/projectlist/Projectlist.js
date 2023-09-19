f4f7249686f44566a22f776db5b3f1716dfafe7d
Pritam Kumar <117884154+01pritam@users.noreply.github.com>
2023-09-19 20:57:34 +0530

    Dock Projectlist.js

--- src/components/projectlist/Projectlist.js
+++ src/components/projectlist/Projectlist.js
@@ -1,6 +1,27 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-
+import './projectlist.css';
+const mockProjects = [
+  {
+    _id: '1',
+    imagePaths: ['https://tse1.mm.bing.net/th?id=OIP.1YM53mG10H_U25iPjop83QHaEo&pid=Api&P=0&h=180', 'path/to/image2.jpg'],
+    title: 'Sample Project 1',
+    description: 'This is a sample project description.',
+    tags: ['tag1', 'tag2'],
+    projectType: 'Type 1',
+    link: 'https://example.com/project1',
+  },
+  {
+    _id: '2',
+    imagePaths: ['https://www.bwallpaperhd.com/wp-content/uploads/2021/01/NashPoint.jpg', 'path/to/image4.jpg'],
+    title: 'Sample Project 2',
+    description: 'Another sample project description.',
+    tags: ['tag3', 'tag4'],
+    projectType: 'Type 2',
+    link: 'https://example.com/project2',
+  },
+  // Add more mock projects as needed
+];
 const ProjectList = ({ authToken }) => {
   const [projects, setProjects] = useState([]);
 
@@ -14,10 +35,11 @@ const ProjectList = ({ authToken }) => {
           },
         });
         setProjects(response.data);
-        // setProjects(profile1.json)
-        
       } catch (error) {
         console.error('Error fetching user projects:', error);
+       // const [projects, setProjects] = useState([mockProjects]);
+       setProjects(mockProjects);
+
       }
     };
 
